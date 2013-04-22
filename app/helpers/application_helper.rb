3a4cc6c200e15cd75b84d4f729fafa47cb215d06
guowei <chuanjiabao1981@gmail.com>
2013-04-22 13:13:08 +0800

    top side bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,18 +3,45 @@ module ApplicationHelper
 	def menu
 		return {
 			main:{
-					primary:{icon: "icon-dashboard", label: "首页",link: root_path},
+					primary:{icon: "icon-home", label: "首页",link: root_path},
 					items: {
-						index: {icon: "icon-dashboard", label: "首页", link: root_path}
+						index: {icon: "icon-home", label: "首页", link: root_path}
 					}
 			},
 			locations: {
-				primary:{icon: "icon-dashboard", label: I18n.t('activerecord.models.location')},
+				primary:{icon: "icon-map-marker", label: I18n.t('activerecord.models.location')},
 				items: {
-					index: {icon: "icon-dashboard", label: I18n.t('activerecord.models.location')+I18n.t('views.text.index'),link: locations_path},
-					new: {icon: "icon-dashboard" ,label:I18n.t('views.text.new') + I18n.t('activerecord.models.location'),link: new_location_path}
+					index: {icon: "icon-th-list", label: I18n.t('activerecord.models.location')+I18n.t('views.text.index'),link: locations_path},
+					new: {icon: "icon-pushpin" ,label:I18n.t('views.text.new') + I18n.t('activerecord.models.location'),link: new_location_path}
 				}
-			}
+			},
+			users:{
+				primary:{icon: "icon-user",label:I18n.t('activerecord.models.user')},
+				items:{
+					index:{icon: 'icon-list-alt',label: I18n.t('activerecord.models.user') + I18n.t('views.text.index'),link: users_path},
+					new: {icon: 'icon-user-md', label: I18n.t('views.text.new') + I18n.t('activerecord.models.user'),link: new_user_path}
+				}
+			},
+			organizations:{
+				primary:{icon: "icon-building",label:I18n.t('activerecord.models.organization')},
+				items:{
+					index:{icon: 'icon-th-large',label: I18n.t('activerecord.models.organization') + I18n.t('views.text.index'),link: organizations_path},
+					new: {icon: 'icon-plus-sign', label: I18n.t('views.text.new') + I18n.t('activerecord.models.organization'),link: new_organization_path}
+				}
+			},
+			quick_reports:{
+				primary:{icon: "icon-pencil",label:I18n.t('activerecord.models.quick_report')},
+				items:{
+					index:{icon: 'icon-list-ol',label: I18n.t('activerecord.models.quick_report') + I18n.t('views.text.index'),link: quick_reports_path},
+					new: {icon: 'icon-magic', label: I18n.t('views.text.new') + I18n.t('activerecord.models.quick_report'),link: new_quick_report_path}
+				}
+			},
+			signout:{
+					primary:{icon: "icon-signout", label: "退出",link: signout_path, method: 'delete'},
+					items: {
+						index: {icon: "icon-signout", label: "退出", link: signout_path}
+					}
+			},
 		}
 	end
 	def nav_active(options = {})
