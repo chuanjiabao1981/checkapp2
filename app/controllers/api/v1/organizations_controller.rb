eacedfd9f9934c9a5360a360ba9972c024356b14
guowei <chuanjiabao1981@gmail.com>
2013-05-07 11:07:08 +0800

    add responsible person and organization

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -4,6 +4,7 @@ module Api
 			def users
 				@users = User.all_subordinates_of_organization(params[:organization_id])
 				@c = @users.collect {|u| [u.name,u.id]}
+				@c.insert(0,["",])
 				render :inline => "<%= options_for_select @c %>"
 			end
 		end
