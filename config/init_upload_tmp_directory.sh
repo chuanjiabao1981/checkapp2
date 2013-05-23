7775d63859246e3181fde53bf78bdbee3f8688c4
guowei <chuanjiabao1981@gmail.com>
2013-05-23 21:52:48 +0800

    tmp rm crontab

--- config/init_upload_tmp_directory.sh
+++ config/init_upload_tmp_directory.sh
@@ -9,7 +9,7 @@ else
 	NGINX_CONF=$1
 	#echo $NGINX_CONF
 fi
-DIR_PREFIX=/tmp/`date +%Y-%m-%d-%s`
+DIR_PREFIX=/tmp/
 sed  -i -e "s#[ ]*upload_store .*#          upload_store $DIR_PREFIX 1 ;#" $NGINX_CONF
 for (( i=0; i<10; i++)); do
 	mkdir -p $DIR_PREFIX/$i
