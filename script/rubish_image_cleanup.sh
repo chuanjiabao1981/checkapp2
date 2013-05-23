a5308d5d759dbff29dcbe736643e3b1267ee43d5
guowei <chuanjiabao1981@gmail.com>
2013-05-23 22:58:20 +0800

    tmp rm crontab

--- script/rubish_image_cleanup.sh
+++ script/rubish_image_cleanup.sh
@@ -4,5 +4,6 @@ _FILE_ABS_DIR=`cd $_FILE_DIR;pwd`
 WORK_DIR=$_FILE_ABS_DIR/../
 
 cd $WORK_DIR
-
+##注意这个目录
+source "/home/work/.rvm/scripts/rvm"
 bundle exec rake images:cleanup RAILS_ENV="production"
