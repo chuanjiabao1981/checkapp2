23019146e887d16b3521ca7cf24f91c093e7ea5f
guowei <chuanjiabao1981@gmail.com>
2013-05-23 21:55:15 +0800

    tmp rm crontab

--- config/init_upload_tmp_directory.sh
+++ config/init_upload_tmp_directory.sh
@@ -1,14 +1,14 @@
-function usage() {
-    echo "usage: $0 nginx conf path"
-}
-
-if [ $# -lt 1 ] ; then
-    usage
-    exit 1
-else
-	NGINX_CONF=$1
-	#echo $NGINX_CONF
-fi
+#function usage() {
+#    echo "usage: $0 nginx conf path"
+#}
+#
+#if [ $# -lt 1 ] ; then
+#    usage
+#    exit 1
+#else
+#	NGINX_CONF=$1
+#	#echo $NGINX_CONF
+#fi
 DIR_PREFIX=/tmp/
 #sed  -i -e "s#[ ]*upload_store .*#          upload_store $DIR_PREFIX 1 ;#" $NGINX_CONF
 for (( i=0; i<10; i++)); do
