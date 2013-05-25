8295cb78963416210f7f3d20b50e4fad64b8caec
guowei <chuanjiabao1981@gmail.com>
2013-05-25 09:29:04 +0800

    user organization refactory first step

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,5 +4,7 @@ class Organization < ActiveRecord::Base
 	validates :manager ,:presence => true
 	belongs_to :tenant
 	belongs_to :manager,:class_name =>"User",:inverse_of => :organization
+	belongs_to :super_organization,:class_name =>"Organization"
+	has_many  :sub_organizations,:class_name => "Organization",:foreign_key => "super_organization_id"
 	default_scope { where(tenant_id: Tenant.current_id)  if Tenant.current_id }
 end
