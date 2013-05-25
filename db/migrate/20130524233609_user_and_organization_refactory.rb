e2388a479629269fccc15e7173927150e5f9c446
guowei <chuanjiabao1981@gmail.com>
2013-05-25 08:32:23 +0800

    user organization refactory first step

---
+++ db/migrate/20130524233609_user_and_organization_refactory.rb
@@ -0,0 +1,43 @@
+class UserAndOrganizationRefactory < ActiveRecord::Migration
+  def up
+  	Organization.all.each do |o|
+  		so = super_organization(o)
+  		if so
+  			print "#{o.name},#{so.name}\n"
+  		else
+  			print "#{o.name},no\n"
+  		end
+  	end
+  	puts "------------------------------------------"
+  	User.all.each do |u|
+  		if u.organization
+  			print "#{u.name},#{u.organization.name}\n"
+  		else
+  			print "#{u.name},#{user_organization(u).try(:name)}\n"
+  		end
+  	end
+  end
+
+  def down
+  end
+
+
+  def user_organization(u)
+  	if u.manager.nil?
+  		return nil
+  	elsif u.manager.organization
+  		return u.manager.organization
+  	else
+  		user_organization(u.manager)
+  	end
+  end
+  def super_organization(o)
+  	if o.manager
+  		if o.manager.try(:manager).try(:organization)
+  			o.manager.manager.organization
+  		else
+  			nil
+  		end
+  	end
+  end
+end
